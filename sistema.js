b30618523262c08f027fe2f6c52ca2489ad04756
SamH037 <samuel.ign01@gmail.com>
2021-08-30 22:33:52 -0300

    Corrigindo comando comentado

--- sistema.js
+++ sistema.js
@@ -1,17 +1,17 @@
-// let numeroDeAlunos = 10;
+let numeroDeAlunos = 10;
 
 // Laço de repetição "for", contado 
-// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
 
-//     if (contador == 0) {
-//         console.log ("O número atual é ZERO");
-//     } else if (contador % 2 == 0) {
-//         console.log(`O número ${contador} é PAR`);
-//     } else {
-//         console.log(`O número ${contador} é ÍMPAR`);
-//     }
+    if (contador == 0) {
+        console.log ("O número atual é ZERO");
+    } else if (contador % 2 == 0) {
+        console.log(`O número ${contador} é PAR`);
+    } else {
+        console.log(`O número ${contador} é ÍMPAR`);
+    }
 
-// }
+}
 
 // Laço de repetição "while", condicional
 
